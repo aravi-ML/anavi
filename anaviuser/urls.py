3da510a6687e6baaff7535b0045171dfec0e7332
LeatitiaAudrey <audreydongmo002@2@gmail.com>
2020-07-28 14:12:07 +0100

    Interfaces de connexions ok

--- anaviuser/urls.py
+++ anaviuser/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name= "anaviuser"
 urlpatterns = [
-    path('login',views.login,name="userlogin")
+    path('login',views.login,name="userlogin"),
+    path('signup',views.signup, name="userSignup"),
+    path('reinitialiser_pwd', views.reinitialiser,name="Reinitialize"),
+    path('reinitialize_mail',views.reinitialise_mail,name="Reinitialze_mail"),
+    path('reinitialise_code',views.reinitialise_code,name="Reinitialise_code")
 ]
